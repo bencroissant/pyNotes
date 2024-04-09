03e9e1a5690529e84287aaad507ec3907f0c9975
BenCroissant <163894113+bencroissant@users.noreply.github.com>
2024-04-09 19:13:34 +0100

    Add files via upload

---
+++ typecast.py
@@ -0,0 +1,13 @@
+# Convert data type of value to another type
+
+x = 1 # integer
+y = 2.0 #float
+z = '3' # string
+
+x = float(x) # replace x with x as float
+y = int(y) #replace y with y as int
+z = int(z) #replace z with z as int
+
+print("X is " + str(x))
+print("Y is " + str(y))
+print("Z is " + str(z*3))
